154c521713cc16701f3ad46dcfb26a51c03c7c9e
zenab mohammed abdelmonaem <78083890+zenab12@users.noreply.github.com>
2021-08-20 15:32:28 +0200

    Add files via upload

---
+++ task 1/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_1
+{
+    class BankAccount
+    {
+        int id;
+        string name;
+        int balance;
+
+        public void SetId(int _id)
+        {
+            id = _id;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetName(string _name)
+        {
+            if(_name.Length > 3)
+               name = _name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void Deposite(int _balance)
+        {
+            if((_balance < 1000000) && (_balance > 50))
+               balance += _balance;
+
+            else
+                Console.WriteLine("this is not enough to deposite in your account ");
+        }
+      
+        public int Withdrawl(int b)
+        {
+            if ((b < balance) && (b > 100))
+                balance -= b;
+            else if ((balance > 100) && (b <= 100))
+                Console.WriteLine("Sorry! You Can't Withdrawl less than 1000");
+            else
+                Console.WriteLine("your account not contain this amount of money");
+          return balance;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Hello {name} , Your ID is {id} and Your Balance in Your Bank ACCount is {balance}");
+
+        }
+
+
+    }
+
+
+  
+}
